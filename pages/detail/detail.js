5e730a3227dd514032e64667b57ff8c8a9513001
锐源 <1370164516@qq.com>
2023-10-26 20:47:17 +0800

    列表跳转到详情页

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -5,14 +5,53 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    films: [
+      {
+        'image':'../../images/1.jpg',
+        'title':'我的战争',
+        'year': '2016',
+        'score': '7.7',
+        'director':'鹏顺',
+        'actor':'王珞丹、刘烨',
+        'id':1
+      },
+      {
+        'image':'../../images/3.png',
+        'title':'河边的错误',
+        'year': '2006',
+        'score': '7.4',
+        'director':'魏淑君',
+        'actor':'朱一龙、侯天来',
+        'id':3
+      },
+      {
+        'image':'../../images/4.png',
+        'title':'传奇一生',
+        'year': '2019',
+        'score': '7.7',
+        'director':'布莱根',
+        'actor':'古道尔',
+        'id':4
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    console.log(options)
+    // id = options.id,
+    wx.setNavigationBarTitle({
+      title: options.title,
+    })
+    // var that = this,
+    // wx.request({
+    //   url: 'url',
+    //   success: function(res){
+    //     that.setData({fim:res.data})
+    //   }
+    // })
   },
 
   /**
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,9 +3,65 @@
 const app = getApp()
 
 Page({
-  detail: function(){
+  data: {
+    films: [
+      {
+        'image':'../../images/1.jpg',
+        'title':'我的战争',
+        'year': '2016',
+        'score': '7.7',
+        'director':'鹏顺',
+        'actor':'王珞丹、刘烨',
+        'id':1
+      },
+      {
+        'image':'../../images/3.png',
+        'title':'河边的错误',
+        'year': '2006',
+        'score': '7.4',
+        'director':'魏淑君',
+        'actor':'朱一龙、侯天来',
+        'id':3
+      },
+      {
+        'image':'../../images/4.png',
+        'title':'传奇一生',
+        'year': '2019',
+        'score': '7.7',
+        'director':'布莱根',
+        'actor':'古道尔',
+        'id':4
+      }
+    ]
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  // url = "https://frodo.douban.com/api/v2/book/rank_list",
+  onLoad(options) {
+    // wx.request({
+    //   url: 'url',
+    //   method: 'GET',
+    //   data: {
+    //     apiKey: '0ac44ae016490db2204ce0a042db'
+    //   },
+    //   header: {
+    //     'User-Agent': 'MicroMessenger', 
+    //     'Referer': 'https://servicewechat.com/wx2f9b06c1de1ccfca/91/page-frame.html',
+    //     "X-AjaxPro-Method":"ShowList",
+    //     success: function (res) {
+    //       console.log(res);
+    //      },
+    //      fail: function (res) {
+    //         console.log(res);
+    //      } 
+    //   },
+    // })
+  },
+  detail: function(res){
+    console.log(res)
     wx.navigateTo({
-      url: '../detail/detail',
+      url: '../detail/detail?id='+res.currentTarget.dataset.id+'&title='+res.currentTarget.dataset.title,
     })
   }
 })
